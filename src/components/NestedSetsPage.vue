403a7af622be0b4c285eae729b5bdbb5bbfc723f
Valerii Platonov <platonov@stem.net.ua>
2018-01-06 12:57:02 +0200

    final converl linear structure to nested tree

--- src/components/NestedSetsPage.vue
+++ src/components/NestedSetsPage.vue
@@ -6,12 +6,12 @@
   import NestedSetsItems from './NestedSetsItems'
   const items = [
     {
-      title: 'Одежда',
+      title: 'Сlothes',
       left: 1,
       right: 22
     },
     {
-      title: 'Мужская',
+      title: 'For men',
       left: 2,
       right: 9
     },
@@ -21,112 +21,44 @@
       right: 21
     },
     {
-      title: 'Костюмы',
+      title: 'Suits',
       left: 3,
       right: 8
     },
     {
-      title: 'Платья',
+      title: 'Dresses',
       left: 11,
       right: 16
     },
     {
-      title: 'Юбки',
+      title: 'Skirts',
       left: 17,
       right: 18
     },
     {
-      title: 'Блузы',
+      title: 'Blouses',
       left: 19,
       right: 20
     },
     {
-      title: 'Брюки',
+      title: 'Pants',
       left: 4,
       right: 5
     },
     {
-      title: 'Жакеты',
+      title: 'Jackets',
       left: 6,
       right: 7
     },
     {
-      title: 'Вечерние',
+      title: 'Evening',
       left: 12,
       right: 13
     },
     {
-      title: 'Летние',
+      title: 'Summer',
       left: 14,
       right: 15
-    }
-  ]
-
-  const nesteditems = [
-    {
-      title: 'Сlothes',
-      left: 1,
-      right: 22,
-      subitems: [
-        {
-          title: 'For men',
-          left: 2,
-          right: 9,
-          subitems: [
-            {
-              title: 'Suits',
-              left: 3,
-              right: 8,
-              subitems: [
-                {
-                  title: 'Pants',
-                  left: 4,
-                  right: 5
-                },
-                {
-                  title: 'Jackets',
-                  left: 6,
-                  right: 7
-                }
-              ]
-            }
-          ]
-        },
-        {
-          title: 'For women',
-          left: 10,
-          right: 21,
-          subitems: [
-            {
-              title: 'Dresses',
-              left: 11,
-              right: 16,
-              subitems: [
-                {
-                  title: 'Evening',
-                  left: 12,
-                  right: 13
-                },
-                {
-                  title: 'Summer',
-                  left: 14,
-                  right: 15
-                }
-              ]
-            },
-            {
-              title: 'Skirts',
-              left: 17,
-              right: 18
-            },
-            {
-              title: 'Blouses',
-              left: 19,
-              right: 20
-            }
-          ]
-        }
-      ]
     }
   ]
 
@@ -137,6 +69,49 @@
     name: 'NestedSetsItems',
     methods: {
       convertItemsStructure (items) {
+        items.sort((a, b) => a.left - b.left)
+
+        function findParent (el, idx, items) {
+          while (--idx >= 0) {
+            if (el.left > items[idx].left && el.right < items[idx].right) {
+              return idx
+            }
+          }
+          return -1
+        }
+
+        function addToParent (a, idx, el) {
+          if (a[idx]) {
+            if (!('subitems' in a[idx])) {
+              a[idx].subitems = {}
+            }
+            a[idx].subitems[el.left] = el
+          }
+          else {
+            for (const i in a) {
+              if ('subitems' in a[i]) {
+                // a[i].subitems=
+                addToParent(a[i].subitems, idx, el)
+              }
+            }
+          }
+
+          return a
+        }
+
+        const result = items.reduce((a, v, i, items) => {
+          const parent = findParent(v, i, items)
+          if (parent === -1) {
+            a[v.left] = v
+          }
+          else {
+            a = addToParent(a, items[parent].left, v)
+          }
+          return a
+        }, {})
+
+        const nesteditems = []
+        for (const k in result) nesteditems.push(result[k])
         return nesteditems
       }
     },
--- test/unit/specs/NestedSetsPage.spec.js
+++ test/unit/specs/NestedSetsPage.spec.js
@@ -3,6 +3,63 @@ import NestedSetsPage from '@/components/NestedSetsPage'
 
 describe('NestedSetsPage.vue', () => {
   let Constructor, vm
+  const items = [
+    {
+      title: 'Сlothes',
+      left: 1,
+      right: 22
+    },
+    {
+      title: 'For men',
+      left: 2,
+      right: 9
+    },
+    {
+      title: 'Женская',
+      left: 10,
+      right: 21
+    },
+    {
+      title: 'Suits',
+      left: 3,
+      right: 8
+    },
+    {
+      title: 'Dresses',
+      left: 11,
+      right: 16
+    },
+    {
+      title: 'Skirts',
+      left: 17,
+      right: 18
+    },
+    {
+      title: 'Blouses',
+      left: 19,
+      right: 20
+    },
+    {
+      title: 'Pants',
+      left: 4,
+      right: 5
+    },
+    {
+      title: 'Jackets',
+      left: 6,
+      right: 7
+    },
+    {
+      title: 'Evening',
+      left: 12,
+      right: 13
+    },
+    {
+      title: 'Summer',
+      left: 14,
+      right: 15
+    }
+  ]
 
   beforeEach(() => {
     Constructor = Vue.extend(NestedSetsPage)
@@ -12,7 +69,7 @@ describe('NestedSetsPage.vue', () => {
     expect(vm.$el).toMatchSnapshot()
   })
   it('Calls method convertItemsStructure', () => {
-    expect(vm.convertItemsStructure([])[0]).toBeInstanceOf(Object)
+    expect(vm.convertItemsStructure([items])[0]).toBeInstanceOf(Object)
   })
   it('should render correct contents', () => {
     expect(vm.$el.querySelector('ul > li').textContent.trim())
